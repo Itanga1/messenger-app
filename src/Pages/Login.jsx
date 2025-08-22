391bbd863e4dfa398b28ed5c6d905079d062a8e9
itanga benigne <itgbenny@gmail.com>
2025-08-22 14:10:57 +0200

    Created the handle  login function

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,13 +1,26 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert("logged in successfully");
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+  console.log(auth.currentUser?.email);
+
   return ( 
     <div className="flex items-center justify-center bg-white min-h-[100vh]">
-      <form className="flex flex-col gap-[5px] px-[30px] py-[40px] bg-white shadow-[-1px_0px_6px_1px_rgba(0,_0,_0,_0.1)] rounded-2xl w-[300px]">
+      <form onSubmit={handleLogin} className="flex flex-col gap-[5px] px-[30px] py-[40px] bg-white shadow-[-1px_0px_6px_1px_rgba(0,_0,_0,_0.1)] rounded-2xl w-[300px]">
         <h1 className="text-center font-bold text-2xl mb-[20px]">Login</h1>
         <label>Email: </label>
         <input required onChange={(e)=>setEmail(e.target.value)} className="px-[20px] py-[5px] border-2 border-green-800 rounded-lg" type="email" placeholder="Enter your email"/>
