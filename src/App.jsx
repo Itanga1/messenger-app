045e369c6e1674188d687f966516d73b8c328cde
itanga benigne <itgbenny@gmail.com>
2025-08-22 11:17:14 +0200

    created the login and signup routes inside the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./Pages/Login"
+import Signup from "./Pages/Signup"
 
+function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/" element={<h1>Welcome to the Messenger App</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
