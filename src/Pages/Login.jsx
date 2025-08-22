b45dd89aa7f59266b8970259cf86139e634677c7
itanga benigne <itgbenny@gmail.com>
2025-08-22 11:13:26 +0200

    Created the login page

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return ( 
+    <div className="flex items-center justify-center bg-white min-h-[100vh]">
+      <form className="flex flex-col gap-[5px] px-[30px] py-[40px] bg-white shadow-[-1px_0px_6px_1px_rgba(0,_0,_0,_0.1)] rounded-2xl w-[300px]">
+        <h1 className="text-center font-bold text-2xl mb-[20px]">Login</h1>
+        <label>Email: </label>
+        <input required onChange={(e)=>setEmail(e.target.value)} className="px-[20px] py-[5px] border-2 border-green-800 rounded-lg" type="email" placeholder="Enter your email"/>
+        <label>Password: </label>
+        <input required onChange={(e)=>setPassword(e.target.value)} className="px-[20px] py-[5px] border-2 border-green-800 rounded-lg" type="password" placeholder="Enter your password"/>
+        <button type="submit" className=" bg-green-800 text-white text-lg py-[5px] rounded-lg my-[10px]">Login</button>
+        <p className="text-center">Don't have account? <Link className="text-green-800 underline" to={'/signup'}>Signup</Link></p>
+      </form>
+    </div>
+   );
+}
+ 
+export default Login;
