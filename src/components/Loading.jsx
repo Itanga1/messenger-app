ea70e06da093e12ceda2891553511ff4c5f7188d
itanga benigne <itgbenny@gmail.com>
2025-08-22 14:11:38 +0200

    created the loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return ( 
+    <div className=" w-full h-[100vh] flex justify-center items-center">
+      <span className="w-[50px] h-[50px] border-[3px] border-gray-300 border-t-green-800 rounded-full animate-spin"></span>
+    </div>
+   );
+}
+ 
+export default Loading;
