877a655f2e0c1ae3412580836267399d74b920ee
itanga benigne <itgbenny@gmail.com>
2025-08-24 10:48:40 +0200

    created the authcontext provide in app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./Pages/Login"
 import Signup from "./Pages/Signup"
 import Home from "./Pages/Home"
+import { AuthContext } from "./Contexts/AuthContext"
+import { useEffect, useState } from "react"
+import { onAuthStateChanged } from "firebase/auth"
+import { auth } from "./config/firebase"
 
 function App() {
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(()=>{
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+    return unsubscribe;
+  },[])
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/" element={<Home/>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider value={{currentUser}}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   )
 }
 
