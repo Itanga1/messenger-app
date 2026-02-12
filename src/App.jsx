f8cf0a21ccc8a835f66a11e16c53af078a137981
itanga benigne <itgbenny@gmail.com>
2026-02-12 19:07:48 +0200

    added the settings route and ThemeContextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Login from "./Pages/Login"
 import Signup from "./Pages/Signup"
 import Home from "./Pages/Home"
 import { AuthContext } from "./Contexts/AuthContext"
+import { ThemeContextProvider } from "./Contexts/ThemeContext.jsx"
+import Settings from "./Pages/Settings"
 import { useEffect, useState } from "react"
 import { onAuthStateChanged } from "firebase/auth"
 import { auth } from "./config/firebase"
@@ -14,26 +16,29 @@ function App() {
   const [currentUser, setCurrentUser] = useState();
   const [loading, setLoading] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
       setLoading(false)
     });
     return unsubscribe;
-  },[])
+  }, [])
   return (
-    <AuthContext.Provider value={{currentUser}}>
-      {loading ? <Loading/> : <BrowserRouter>
-        <Routes>
-          <Route element={<UnProtectedRoutes/>}>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/signup" element={<Signup/>}/>
-          </Route>
-          <Route element={<ProtectedRoutes/>}>
-            <Route path="/" element={<Home/>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>}
+    <AuthContext.Provider value={{ currentUser }}>
+      <ThemeContextProvider>
+        {loading ? <Loading /> : <BrowserRouter>
+          <Routes>
+            <Route element={<UnProtectedRoutes />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Route>
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/settings" element={<Settings />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>}
+      </ThemeContextProvider>
     </AuthContext.Provider>
   )
 }
